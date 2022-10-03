7413816c5d13c267a4da58b96d2c8a9689d145ac
Mohit Varma <70433964+n0IQ@users.noreply.github.com>
2022-10-04 01:04:34 +0530

    Update dsu.cpp

--- Disjoint Set Union (DSU)/dsu.cpp
+++ Disjoint Set Union (DSU)/dsu.cpp
@@ -1,30 +1,25 @@
-const int N = 1e5;
-int p[N + 5], r[N + 5]; // parent array and rank array
-
-void init()
-{
-    for (int i = 0; i < N; i++)
-        p[i] = i, r[i] = 1;
-}
-
-int find_set(int x)
-{
-    if (x == p[x]) 
-        return x;
-    
-    return p[x] = find_set(p[x]);
-}
-
-void union_set(int a, int b)
-{
-    a = find_set(a);
-    b = find_set(b);
-    
-    if (a != b) {
-        if (r[a] < r[b]) 
-            swap(a, b);
-        
-        p[b] = a;
-        r[a] += r[b];
-    }
-}
+struct DSU {
+	vector<int> p, r; // parent and rank array
+ 
+	DSU(int n) {
+		p.assign(n + 1, 0);
+		iota(p.begin(), p.end(), 0);
+		r.assign(n + 1, 1);
+	}
+ 
+	int find_set(int x)
+	{
+		if (x == p[x]) return x;
+		return p[x] = find_set(p[x]);
+	}
+ 
+	bool union_set(int a, int b)
+	{
+		a = find_set(a); b = find_set(b);
+		if (a == b) return false;
+		if (r[a] < r[b]) swap(a, b);
+		p[b] = a;
+		r[a] += r[b];
+		return true;
+	}
+};
